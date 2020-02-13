73d829c5f099da30b3d820ed823e401f9b0a7037
Pradeep V <pradeepkumar.vaddi@gmail.com>
2020-02-13 19:57:29 +0530

    Initial Commit

---
+++ ProofOfConcept/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProofOfConcept
+//
+//  Created by Pradeep Kumar Vadde (Digital) on 13/02/20.
+//  Copyright © 2020 Wipro Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
