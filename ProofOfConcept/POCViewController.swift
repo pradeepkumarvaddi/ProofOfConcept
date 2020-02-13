d2218e1aec034b74d9e5c74f0f2d95b531e225d8
Pradeep V <pradeepkumar.vaddi@gmail.com>
2020-02-13 20:09:33 +0530

    Deleted Main.Stroyboard and created POCViewController. Adding third party libraries using cocoa pods

---
+++ ProofOfConcept/POCViewController.swift
@@ -0,0 +1,17 @@
+//
+//  POCViewController.swift
+//  ProofOfConcept
+//
+//  Created by Pradeep Kumar Vadde (Digital) on 13/02/20.
+//  Copyright © 2020 Wipro Technologies. All rights reserved.
+//
+
+import UIKit
+
+class POCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
